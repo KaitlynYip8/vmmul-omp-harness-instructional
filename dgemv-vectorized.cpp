e3ed045c16066c2f7add7f3eb43a463d85b2c652
Kaitlyn <kaitlynyip8@gmail.com>
2025-04-24 16:15:55 -0700

    Worked on vectorized code

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -10,8 +10,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
    for(int i = 0; i < n; i++) {
       for(int j = 0; j < n; j++) {
-         int index = (i * n) + j;
-         y[i] = y[i] + A[index] * x[j];
+         y[i] = y[i] + A[(i * n) + j] * x[j];
       }
    }
 }
