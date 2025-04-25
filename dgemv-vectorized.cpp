80639fc814966fb7001e2623b1c820f122ac0e96
Kaitlyn <kaitlynyip8@gmail.com>
2025-04-25 16:19:52 -0700

    Changed index to i /n

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -11,16 +11,14 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    int index = 0;
    int* P = new int[n];
   // int value = 0;
-   for(int i = 0; i < n; i++) {
-      index = i * n;
-      for(int j = 0; j < n; j++) {
-      P[i] += (A[index] * x[j]);
-         index++;
+   for(int i = 0; i < n * n; i++) {
+      index = i / n;
+      int j = i % n;
+      P[index] += (A[i] * x[j]);
       //    y[(i + 1)/n] = y[(i + 1)/n] + A[(i + 1)] * x[(i + 1) % n];
       //    y[(i + 2)/n] = y[(i + 2)/n] + A[(i + 2)] * x[(i + 2) % n];
       //    y[(i + 3)/n] = y[(i + 3)/n] + A[(i + 3)] * x[(i + 3) % n];
       //    y[(i + 4)/n] = y[(i + 4)/n] + A[(i + 4)] * x[(i + 4) % n];
-      }
    }
    for(int i = 0; i < n; i++) {
       y[i] += P[i];
