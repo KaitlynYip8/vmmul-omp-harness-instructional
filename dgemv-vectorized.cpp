1730468a6912b1e1873e7ef88ce306daebbbc502
Kaitlyn <kaitlynyip8@gmail.com>
2025-04-25 15:24:31 -0700

    Added indexes

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,10 +8,12 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
+   int index = 0;
    for(int i = 0; i < n; i++) {
+      index = i * n;
       for(int j = 0; j < n; j++) {
-      int index = (i * n) + j;
          y[i] = y[i] + A[index] * x[j];
+         index++;
       //    y[(i + 1)/n] = y[(i + 1)/n] + A[(i + 1)] * x[(i + 1) % n];
       //    y[(i + 2)/n] = y[(i + 2)/n] + A[(i + 2)] * x[(i + 2) % n];
       //    y[(i + 3)/n] = y[(i + 3)/n] + A[(i + 3)] * x[(i + 3) % n];
