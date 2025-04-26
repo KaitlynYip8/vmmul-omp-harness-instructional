c085a6b747e4670b8ddae1a5312ca7421ff12dcb
Kaitlyn <kaitlynyip8@gmail.com>
2025-04-25 17:48:45 -0700

    Added vectorized code to openmp

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -25,13 +25,23 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you will want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
+   int index = 0;
+   int* P = new int[n];
+  // int value = 0;
+  #pragma omp parallel for
+   for(int i = 0; i < n * n; i++) {
+      index = i / n;
+      int j = i % n;
+      P[index] += (A[i] * x[j]);
+      //    y[(i + 1)/n] = y[(i + 1)/n] + A[(i + 1)] * x[(i + 1) % n];
+      //    y[(i + 2)/n] = y[(i + 2)/n] + A[(i + 2)] * x[(i + 2) % n];
+      //    y[(i + 3)/n] = y[(i + 3)/n] + A[(i + 3)] * x[(i + 3) % n];
+      //    y[(i + 4)/n] = y[(i + 4)/n] + A[(i + 4)] * x[(i + 4) % n];
+   }
    #pragma omp parallel for
    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < n; j++) {
-         int index = (i * n) + j;
-         y[i] = y[i] + (A[index] * x[j]);
-      }
+      y[i] += P[i];
    }
-
+   delete[] P;
 }
 
