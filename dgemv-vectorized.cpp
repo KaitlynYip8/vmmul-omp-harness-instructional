8f4d5a2ca703194f5adc89659af3f2f91afc081b
Kaitlyn <kaitlynyip8@gmail.com>
2025-04-28 16:32:17 -0700

    set sum to y[j]

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -9,7 +9,7 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
    for(int j = 0; j < n; j++) {
-      int sum = 0;
+      int sum = y[j];
       int index = j * n;
       for(int i = 0; i < n; i++) {
          sum += (A[index + i] * x[i]);
