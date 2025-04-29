76cc467232fcd8037fa7f7994dfbe2420219de52
Kaitlyn <kaitlynyip8@gmail.com>
2025-04-28 17:06:16 -0700

    += sum

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -11,12 +11,11 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
    for(int i = 0; i < n; i++) {
       int index = i * n;
-      printf("Y: %f", y[i]);
-      int sum = y[i];
+      int sum = 0;
      // y[i] = 0;
       for(int j = 0; j < n; j++) {
-         sum += (A[index + j] * x[j]);
+         sum = (A[index + j] * x[j]);
+         y[i] += sum;
       }
-      y[i] += sum;
    }
 }
