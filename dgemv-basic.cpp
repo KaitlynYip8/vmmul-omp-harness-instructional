e3258d2349e36ffda533c2813af25f6ea20723a9
Kaitlyn <kaitlynyip8@gmail.com>
2025-04-28 17:09:39 -0700

    set sum to 0

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -11,7 +11,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
    for(int i = 0; i < n; i++) {
       int index = i * n;
-      double sum = y[i];
+      double sum = 0;
      // y[i] = 0;
       for(int j = 0; j < n; j++) {
          sum += (A[index + j] * x[j]);
